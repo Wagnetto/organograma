06fd8996dc334526b5510e8146ff372cb6694e63
Wagner Silveira Souto <wagner.souto@ifood.com.br>
2022-10-06 09:19:30 -0300

    Creates team list and its states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([]);
+  const aoAdicionar = (colaborador) => {
+    
+    setColaboradores([...colaboradores, colaborador])
+    console.log(colaboradores)
+  }
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoCadastrar={colaborador => aoAdicionar(colaborador)} />
     </div>
   );
 }
